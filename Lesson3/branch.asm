e75a289f1d89e3de97ed8103b8cda75a8c881c8f
Matt Heffernan <slithymatt@hotmail.com>
2021-01-02 03:18:14 -0500

    finished slides, example program, and first part of script

--- Lesson3/branch.asm
+++ Lesson3/branch.asm
@@ -4,26 +4,62 @@
 .segment "ONCE"
 .segment "CODE"
 
-   jmp start         ; absolute
+CHROUT = $FFD2
+NEWLINE = $0D
+
+   jmp start
 
 data:
 .byte $01,$23,$45,$67,$89,$AB,$CD,$EF
 
-results:
-.byte 0,0,0
-
 start:
-   lda #$C1
-   pha
-   plp
+   ; branching if less than (N set)
+   ldx #0
+n_loop:
+   lda data,x
+   cmp #$55
+   bmi next_n
+   txa
+   ora #$30
+   jsr CHROUT
+next_n:
+   inx
+   cpx #8
+   bne n_loop
+   lda #NEWLINE
+   jsr CHROUT
+
+   ; branching if carry (C set)
+   ldx #0
+c_loop:
+   lda data,x
    clc
-   clv
-   lda #0
-   adc #127
-   adc #1
-   adc #127
-   adc #1
+   adc #$55
+   bcs next_c
+   txa
+   ora #$30
+   jsr CHROUT
+next_c:
+   inx
+   cpx #8
+   bne c_loop
+   lda #NEWLINE
+   jsr CHROUT
 
-
-return:
+   ; branching if overflow (V set)
+   ldx #0
+v_loop:
+   lda data,x
+   clc
+   adc #$55
+   bvs next_v
+   txa
+   ora #$30
+   jsr CHROUT
+next_v:
+   inx
+   cpx #8
+   bne v_loop
+   lda #NEWLINE
+   jsr CHROUT
    rts
