2a28e02a048d86b927136468c84a4578dd2828b0
Matt Heffernan <slithymatt@hotmail.com>
2021-02-16 02:11:26 -0500

    finished Lesson 9 slides, started program

--- Lesson9/vera.asm
+++ Lesson9/vera.asm
@@ -19,12 +19,64 @@ VERA_L1_mapbase   = $9F35
 VERA_L1_tilebase  = $9F36
 
 ; VRAM Addresses
+VRAM_layer1_map   = $00000
+VRAM_layer0_map   = $04000
+VRAM_lowerchars   = $0F000
 VRAM_petscii      = $0F800
 VRAM_palette      = $1FA00
 
+; Kernal
+CHROUT            = $FFD2
+
+; PETSCII Codes
+RETURN            = $0D
+CLR               = $93
+
    jmp start
 
+smile:
+.byte %00111100
+.byte %01000010
+.byte %10100101
+.byte %10000001
+.byte %10100101
+.byte %10011001
+.byte %01000010
+.byte %00111100
+
+email: .asciiz "matt@slithygames.com"
+hidden: .asciiz "SECRET MESSAGE!"
 
 start:
+   ; clear screen
+   lda #CLR
+   jsr CHROUT
+
+   ; print text lines
+   ldx #0
+@email_loop:
+   lda email,x
+   beq @done_email
+   jsr CHROUT
+   inx
+   bra @email_loop
+@done_email:
+   lda #RETURN
+   jsr CHROUT
+   ldx #0
+@hidden_loop:
+   lda hidden,x
+   beq @done_hidden
+   jsr CHROUT
+   inx
+   bra @hidden_loop
+@done_hidden:
+   lda #RETURN
+   jsr CHROUT
+
+   ; Copy Lower/Upper character set from ROM to VRAM
+   
+
+
 
    rts
