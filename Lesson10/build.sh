8d7145f84e06e292cafa6525b0c1bb6961c1c9b7
Matt Heffernan <slithymatt@hotmail.com>
2021-02-23 12:14:54 -0500

    started lesson 10

---
+++ Lesson10/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cl65 -t cx16 -o ISR.PRG -l isr.list isr.asm
---
+++ Lesson10/isr.asm
@@ -0,0 +1,14 @@
+.org $080D
+.segment "STARTUP"
+.segment "INIT"
+.segment "ONCE"
+.segment "CODE"
+
+   jmp start
+
+
+
+start:
+
+   rts
+   
