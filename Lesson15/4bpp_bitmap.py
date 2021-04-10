822b6f314254a0e8b0687f534260dc20693d015c
Matt Heffernan <slithymatt@hotmail.com>
2021-04-10 01:36:59 -0400

    final draft of Lesson 15 script

--- Lesson15/4bpp_bitmap.py
+++ Lesson15/4bpp_bitmap.py
@@ -5,15 +5,15 @@ f = open(sys.argv[1], "rb")
 iData = bytearray(f.read()) # read to array
 f.close()
 
-# open bitmap output file, initialize with address header
-f = open("BITMAP.BIN", "wb")
+# create new byte array initialized with address header
 oData = bytearray([0,0]) # header
 
 # consolidate pixels to 4bpp
 for i in range(0,int(len(iData)/2)):
     oData.append((iData[i*2] << 4) | iData[i*2+1])
 
-# write bitmap file
+# write new bytearray to output bitmap file
+f = open("BITMAP.BIN", "wb")
 f.write(oData)
 f.close()
 
@@ -22,18 +22,18 @@ f = open(sys.argv[1] + ".pal", "rb")
 iData = bytearray(f.read()) # read to array
 f.close()
 
-# open palette output file, initialize with address header
-f = open("PAL.BIN", "wb")
+# create new byte array initialized with address header
 oData = bytearray([0,0]) # header
 
 # truncate colors to 12-bit
 for i in range(0,int(len(iData)/3)):
-    b = iData[i*3+1] & 0xF0 # blue
-    b = b | (iData[i*3+2] >> 4) # green
-    oData.append(b) #BG
+    b = iData[i*3+1] & 0xF0 # green
+    b = b | (iData[i*3+2] >> 4) # blue
+    oData.append(b) #GB
     b = iData[i*3] >> 4 # red
     oData.append(b) #0R
 
-# write palette file
+# write new bytearray to output palette file
+f = open("PAL.BIN", "wb")
 f.write(oData)
 f.close()
